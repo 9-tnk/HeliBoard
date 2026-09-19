c8df7640c0687fcc2000ccc8216984cf806214aa
9-tnk <thanakrit130149@gmail.com>
2026-09-19 07:35:57 +0700

    Refactor condition to avoid sending spaces in languages

--- app/src/main/java/helium314/keyboard/latin/inputlogic/InputLogic.java
+++ app/src/main/java/helium314/keyboard/latin/inputlogic/InputLogic.java
@@ -1149,10 +1149,10 @@ public final class InputLogic {
         final int codePoint = event.getCodePoint();
         final SettingsValues settingsValues = inputTransaction.getSettingsValues();
         final boolean wasComposingWord = mWordComposer.isComposingWord();
-        // We avoid sending spaces in languages without spaces if we were composing.
         final boolean shouldAvoidSendingCode = Constants.CODE_SPACE == codePoint
-                && !settingsValues.mSpacingAndPunctuations.mCurrentLanguageHasSpaces
-                && wasComposingWord;
+        && !settingsValues.mSpacingAndPunctuations.mCurrentLanguageHasSpaces
+        && wasComposingWord
+        && !"th".equals(settingsValues.mLocale.getLanguage());
 
         if (mWordComposer.isCursorFrontOrMiddleOfComposingWord()) {
             // If we are in the middle of a recorrection, we need to commit the recorrection
