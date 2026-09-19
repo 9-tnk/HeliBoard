acaf46c1a0352d0d9d27d21ca324fc86ccf2ced9
9-tnk <thanakrit130149@gmail.com>
2026-09-19 07:44:38 +0700

    Fix condition for avoiding code sending on space

--- app/src/main/java/helium314/keyboard/latin/inputlogic/InputLogic.java
+++ app/src/main/java/helium314/keyboard/latin/inputlogic/InputLogic.java
@@ -1150,9 +1150,9 @@ public final class InputLogic {
         final SettingsValues settingsValues = inputTransaction.getSettingsValues();
         final boolean wasComposingWord = mWordComposer.isComposingWord();
         final boolean shouldAvoidSendingCode = Constants.CODE_SPACE == codePoint
-        && !settingsValues.mSpacingAndPunctuations.mCurrentLanguageHasSpaces
-        && wasComposingWord
-        && !"th".equals(settingsValues.mLocale.getLanguage());
+                && !settingsValues.mSpacingAndPunctuations.mCurrentLanguageHasSpaces
+                && wasComposingWord
+                && !"th".equals(settingsValues.mLocale.getLanguage());
 
         if (mWordComposer.isCursorFrontOrMiddleOfComposingWord()) {
             // If we are in the middle of a recorrection, we need to commit the recorrection
